805755e2f5ad9c416c7748dc4cdf954f2dc8869b
Ulises Cervantes <ulises.cerv@hotmail.com>
2020-11-24 15:12:46 -0600

    FEATURE:  Add the Router functionality

--- src/App.js
+++ src/App.js
@@ -2,16 +2,29 @@
 import './App.css';
 import Home from './Components/Home/Home';
 import Header from './Components/Header/Header';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      {/* HEADER */}
-      <Header />
-      {/* HOME */}
-      <Home />
+    <Router>
+      <div className="App">
 
-    </div>
+        <Switch>
+
+          <Route path='/checkout'>
+            <h1>Checkout</h1>
+          </Route>
+
+          {/* Make sure the Default route is on the bottom */}
+          <Route path='/'>
+            <Header />
+            <Home />
+          </Route>
+
+        </Switch>
+
+      </div>
+    </Router>
   );
 }
 
