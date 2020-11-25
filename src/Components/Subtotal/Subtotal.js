b1a10f14e1c79d18670f3a050a3bb4b86157cbf4
Ulises Cervantes <ulises.cerv@hotmail.com>
2020-11-25 09:50:21 -0600

    FEATURE: CHECKOUT Comp

--- src/Components/Subtotal/Subtotal.js
+++ src/Components/Subtotal/Subtotal.js
@@ -5,8 +5,25 @@ import CurrencyFormat from 'react-currency-format'
 export default function Subtotal() {
     return (
         <div className='subtotal'>
-
-
+            <CurrencyFormat
+                renderText={value => (
+                    <>
+                        <p>
+                            Subtotal (0 items): <strong>{0}</strong>
+                        </p>
+                        <small className="subtotal__gif">
+                            <input type="checkbox" />
+                            This order contains a gift
+                        </small>
+                    </>
+                )}
+                decimalScale={2}
+                value={0}
+                displayType={'text'}
+                thousandSeparator={true}
+                prefix={'$'}
+            />
+            <button>Proceed to Checkout</button>
         </div>
     )
 }
